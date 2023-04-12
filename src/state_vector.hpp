90f7c77a1e9fbdd88595eccebbdb8a0f858f9999
Koki Kawamura <kowerkoint@gmail.com>
2023-04-12 10:37:53 +0900

    japanese doxygen comment

--- src/state_vector.hpp
+++ src/state_vector.hpp
@@ -26,6 +26,7 @@ struct StateVectorData<StateVectorImplementation::DEFAULT> {
 
 /**
  * @brief StateVector expression of quantum state
+ * \~japanese-en 量子状態の状態ベクトルによる表現
  */
 template <StateVectorImplementation implementation>
 class StateVector {
@@ -37,26 +38,31 @@ private:
 public:
     /**
      * @brief num of qubits
+     * \~japanese-en 量子ビット数
      */
     const UINT& qubit_count;
 
     /**
      * @brief dimension of state vector
+     * \~japanese-en 量子状態の次元
      */
     const ITYPE& dim;
 
     /**
      * @brief classical register
+     * \~japanese-en 古典ビットのレジスタ
      */
     const std::vector<UINT>& classical_register;
 
     /**
      * @brief data
+     * \~japanese-en 状態ベクトルのデータ
      */
     StateVectorData<implementation> data;
 
     /**
      * @brief constructor
+     * \~japanese-en コンストラクタ
      * @param qubit_count num of qubits
      */
     StateVector(UINT qubit_count_);
